5e2230f37424e5259de8da8bbb59fdf6e29f4bff
DaniilYusha <ilushenko79@gmail.com>
2020-08-20 17:07:18 +0300

    Add autoloader file

---
+++ autoload.rb
@@ -0,0 +1,11 @@
+require 'i18n'
+require 'erb'
+require 'tilt'
+require 'delegate'
+require 'codebreaker'
+
+require_relative 'config/i18n'
+
+require_relative 'app/helpers/data_management_helper'
+
+require_relative 'app/middlewares/web_application'
