c1ed3063e4407a6dab65501ec6b49853575093e2
DaniilYusha <ilushenko79@gmail.com>
2020-08-21 00:42:18 +0300

    Require all files in autoload file

--- autoload.rb
+++ autoload.rb
@@ -1,11 +1,13 @@
 require 'i18n'
 require 'erb'
+require 'yaml'
 require 'tilt'
 require 'delegate'
 require 'codebreaker'
 
 require_relative 'config/i18n'
 
-require_relative 'app/helpers/data_management_helper'
+require_relative 'app/codebreaker_web/helpers/data_management_helper'
+require_relative 'app/codebreaker_web/helpers/game_storage_helper'
 
-require_relative 'app/middlewares/web_application'
+require_relative 'app/codebreaker_web/web_application'
